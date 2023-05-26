3044e461170fb25ad3acba6794f48a5c7a4ae2a4
gabsayuri <gabriellesayuri13@gmail.com>
2023-05-26 21:24:24 +0900

    atualizacao26/05

--- www/motor.js
+++ www/motor.js
@@ -1,11 +1,31 @@
 var nome
 var selecao
 var selecaodisciplina
+var selecaomencao
 
-function cadastrar() {
+function enviar() {
     nome = document.getElementById('inputnome').value
     document.getElementById('nome').textContent = nome
 
     selecao = document.getElementById('disciplina')
     selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    selecao = document.getElementById('mencao')
+    selecaomencao = selecao.options[selecao.selectedIndex].value
+
+    localStorage.setItem(selecaodisciplina. selecaomencao)
+
+}
+
+function listar() {
+  var exibirdisciplina
+  var exibirmencao
+
+  selecao = document.getElementById('disciplina')
+  selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+  exibirmencao = localStorage.getItem(selecaodisciplina)
+
+  document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+  document.getElementById('exibirmencao').textContent = exibirmencao
 }
